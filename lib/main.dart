1d226e0b81e55dcd42816aa82e85fd8359e85d0d
Juncen Li <li.junce@norhteastern.edu>
2022-11-21 13:15:45 -0800

    refined

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text('I Am Rich'),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      body: const Center(
-        child: Image(
-          image: AssetImage('assets/images/diamond.png'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.black,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/images/diamond.png'),
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
+
+
